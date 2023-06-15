4706da748628c407a8401f5b7859136ec9d7e707
fluke <flukebevil@gmail.com>
2023-06-15 09:21:42 +0700

    chore: setup prettier

--- src/conponents/Hello.tsx
+++ src/conponents/Hello.tsx
@@ -5,7 +5,7 @@ import { GithubService } from '../services/demo.service';
 export type Props = {
   name: string;
   baseEnthusiasmLevel?: number;
-}
+};
 
 const Hello: React.FC<Props> = ({ name, baseEnthusiasmLevel = 2 }) => {
   const [enthusiasmLevel, setEnthusiasmLevel] = useState(baseEnthusiasmLevel);
@@ -15,26 +15,26 @@ const Hello: React.FC<Props> = ({ name, baseEnthusiasmLevel = 2 }) => {
   useEffect(() => {
     if (!datas) {
       service.list().then((res) => {
-        console.log(res.data)
-        setDatas(res.data)
-      }).catch(() => { });
+        console.log(res.data);
+        setDatas(res.data);
+      });
     }
-  }, [datas])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [datas]);
 
   const onIncrement = () => setEnthusiasmLevel(enthusiasmLevel + 1);
   const onDecrement = () => setEnthusiasmLevel(enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0);
-  const getExclamationMarks = (numChars: number) => numChars > 0 ? Array(numChars + 1).join('!') : '';
+  const getExclamationMarks = (numChars: number) =>
+    numChars > 0 ? Array(numChars + 1).join('!') : '';
 
   return (
     <View>
       <Text style={styles.greeting}>
-        Hello{' '}
-        {name}{datas?.msg}
+        Hello {name}
+        {datas?.msg}
         {getExclamationMarks(enthusiasmLevel)}
       </Text>
-      <Text>
-        Data from fetched api {datas?.msg}
-      </Text>
+      <Text>Data from fetched api {datas?.msg}</Text>
       <View>
         <Button
           title="Increase enthusiasm"
@@ -56,8 +56,8 @@ const Hello: React.FC<Props> = ({ name, baseEnthusiasmLevel = 2 }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   greeting: {
     fontSize: 20,
